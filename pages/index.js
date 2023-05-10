ff1817c1895288012d4f0059e72bcd162beecc7f
Sonali Marlin <967108@tuffshed.com>
2023-05-10 17:16:17 -0600

    add static server side rendering on home page

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,44 @@
-import { getFeaturedEvents } from "../dummy-data";
+// import { getFeaturedEvents } from "../dummy-data";
+import { getFeaturedEvents } from "../helpers/api-util";
 import EventList from "../components/events/event-list";
+// import { useEffect } from "react";
+// import useSWR from 'swr';
 
-export default function HomePage() {
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+export default function HomePage(props) {
+  /*
+  Was previously fectched from dummy-data file
   const featuredEvents = getFeaturedEvents();
+  */
+
+  /* 
+  Client-side fetching of data with useEffect
+  useEffect(() => {
+    console.log('running')
+  }, [])
+  */
+
+  /* 
+  Client-side fetching of data with SWR
+  */
 
   return (
     <>
       <h1 className="center">Featured Events!</h1>
-      <EventList events={featuredEvents} />
+      <EventList events={props.events} />
     </>
   );
+}
+
+/*
+Static server-side rendering of the page.
+*/
+export async function getStaticProps(context) {
+  const featuredEvents = await getFeaturedEvents();
+  return {
+    props: {
+      events: featuredEvents,
+    },
+  };
 }
